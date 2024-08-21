bf1c235b9cee29256907b22668fceee4319f09b1
ajmm26 <ajmm9257@gmail.com>
2024-08-21 16:51:11 -0300

    sector de contacto, funciomiento del morpher y zona proyecto responsive

--- public/js/JS.js
+++ public/js/JS.js
@@ -1,5 +1,8 @@
 
 const boton = document.getElementById('morpher');
+const imagen = document.getElementById('img-ranger');
+const superponer=document.getElementById('div-superponer');
+const contenedorMorpher=document.getElementById('div-morpher');
 
 boton.addEventListener('click', function() {
     // Llamar a blur() para quitar el foco del botón
@@ -10,6 +13,9 @@ boton.addEventListener('click', function() {
 });
 
 function mopher() {
+    createButtonoFF();
+    addClass();
+    changeImg();
     // Crear y reproducir el primer sonido
     const sound = document.createElement('audio');
     sound.src = 'Sound/sound_morphin.mp3';
@@ -31,10 +37,62 @@ function segundoSonido() {
     const audio = document.createElement('audio');
     audio.src = 'Sound/morphin-time.mp3';
     document.body.appendChild(audio);
+    audio.volume=1;
     audio.play();
 
     // Quitar el elemento del DOM después de reproducirlo
     audio.addEventListener('ended', () => {
         document.body.removeChild(audio);
     });
+}
+
+function addClass(){
+
+    superponer.classList.add('superponer');
+    setTimeout(() => {
+        superponer.classList.remove('superponer');
+    }, 7000); // Ajusta el tiempo según tus necesidades
+}
+
+function changeImg(){
+
+    setTimeout(()=>{
+
+        imagen.src='img/Morphin.webp';
+
+    }, 7000)
+
+
+}
+
+
+
+
+/*Creacion de boton que desactiva los poderes*/
+
+function createButtonoFF(){
+
+    const buttonOff=document.createElement('button');
+    
+    
+    setTimeout(()=>{
+        buttonOff.id= 'morpher';
+        buttonOff.innerText='Power OFF';
+        contenedorMorpher.style.marginLeft= '19%'
+        boton.remove();
+        contenedorMorpher.appendChild(buttonOff);
+    }, 7000)
+
+
+    buttonOff.addEventListener('click',()=>{
+     buttonOff.remove();
+     imagen.src='img/Myself.png'
+     contenedorMorpher.appendChild(boton)
+     contenedorMorpher.style.marginLeft= '0px';
+
+
+    })
+
+
+
 }
